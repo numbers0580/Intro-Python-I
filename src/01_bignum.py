86fc4a4de169a2433897f1bd38e41911e9f72433
numbers0580 <koreankid18@yahoo.com>
2020-08-31 14:01:02 -0600

    0, 1, 2 complete. Rest in progress.

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,10 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+print("I'm asked to print 2 to the 65536 power here.")
+print ("Mathematician's Note: This will get ugly. 2^10 is ~1000, 2^20 is ~1 million, 2^30 is ~1 billion, etc.", "At that rate, we're expecting a value that has about 20,000 zeroes.")
+print(f"Here goes: {2 ** 65536}")
+
+print("Or another way:")
+toobig = 2 ** 65536
+print(toobig)
